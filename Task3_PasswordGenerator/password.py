c3ee6795647be98d79b4a2c63e7bbf3bfebb97ae
yadavallianusha <yadavallianusha019@gmail.com>
2026-02-19 08:24:49 +0530

    Create password.py

---
+++ Task3_PasswordGenerator/password.py
@@ -0,0 +1,9 @@
+import random
+import string
+length=int(input("enthe the lenth:"))
+characters=string.ascii_letters + string.digits + string.punctuation
+password=""
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
