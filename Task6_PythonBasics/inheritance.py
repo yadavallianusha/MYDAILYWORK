e97aa0c536b2ac39cfbff5e13a5cdc3d90f93832
Yadavalli Anusha <yadavallianusha019@gmail.com>
2026-02-26 08:35:37 +0530

    Update inheritance.py

--- Task6_PythonBasics/inheritance.py
+++ Task6_PythonBasics/inheritance.py
@@ -1,13 +1,15 @@
 class Animal:
-    def __init__(self, name ,age):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
     def speak(self):
         print(self.name, "makes a sound")
-      print("age:" self.age)
+        print("Age:", self.age)
 class Dog(Animal):
     def speak(self):
         print(self.name, "barks")
-animal1 = Animal("Generic Animal")
-dog1 = Dog("Rex",4)
+        print("Age:", self.age)
+animal1 = Animal("Generic Animal", 5)
+dog1 = Dog("Rex", 4)
 animal1.speak()
 dog1.speak()
