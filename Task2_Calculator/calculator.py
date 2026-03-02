6040a0fe69759de925cd2d4f928df9d2d223d076
Yadavalli Anusha <yadavallianusha019@gmail.com>
2026-03-02 08:24:40 +0530

    Update calculator.py

--- Task2_Calculator/calculator.py
+++ Task2_Calculator/calculator.py
@@ -10,12 +10,16 @@ print("4. Division")
 choice = input("Enter choice (1/2/3/4): ")
 
 if choice == "1":
-    print("Result:", num1 + num2)
+    print("Result:", num1 + num2) # add the num
 elif choice == "2":
-    print("Result:", num1 - num2)
+    print("Result:", num1 - num2) # Subtract two numbers
 elif choice == "3":
-    print("Result:", num1 * num2)
+    print("Result:", num1 * num2)# Multiply two numbers
+
 elif choice == "4":
-    print("Result:", num1 / num2)
+    if num2 != 0:
+        print("Result:", num1 / num2)  # Divide two numbers
+    else:
+        print("Error: Division by zero is not allowed")
 else:
     print("Invalid choice")
