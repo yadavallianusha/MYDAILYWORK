888ca8763a86c5f3c2b954789ae53d4a4c60afd6
Yadavalli Anusha <yadavallianusha019@gmail.com>
2026-03-02 08:40:22 +0530

    Update weather.py

--- Task4_WeatherForecast/weather.py
+++ Task4_WeatherForecast/weather.py
@@ -1,15 +1,17 @@
-import requests
+import requests   # Import requests library to make HTTP requests
 
-API_KEY = input("Enter YOUR_API_KEY: ")
+API_KEY = input("Enter YOUR_API_KEY: ")   # enter the api key
 
-city = input("Enter city: ")
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-
+city = input("Enter city: ")   # enter city
+# Create the API request URL with city, API key, and metric units
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"  
+# Send GET request to the weather API
 res = requests.get(url)
+# Convert response data to JSON format
 data = res.json()
-
+# Check if the request was successful
 if res.status_code == 200:
-    print("City:", data["name"])
-    print("Temp:", data["main"]["temp"], "°C")
+    print("City:", data["name"])  # Print city name
+    print("Temp:", data["main"]["temp"], "°C")  # Print temperature in Celsius
 else:
-    print("City not found")
+    print("City not found")  # Error message if city is not found
