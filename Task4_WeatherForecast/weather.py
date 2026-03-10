b167affb6f3d1a79d9865eff8b545dea37346fbc
Yadavalli Anusha <yadavallianusha019@gmail.com>
2026-03-10 07:48:46 +0530

    Update weather.py

--- Task4_WeatherForecast/weather.py
+++ Task4_WeatherForecast/weather.py
@@ -1,18 +1,12 @@
 import requests   # Import requests library to make HTTP requests
-
 API_KEY ="eb867404243498ab95344ec940642ba7"   # Your API key
-
 city = "Bangalore"  # city name
-
 # Create the API request URL with city, API key, and metric units
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-
 # Send GET request to the weather API
 res = requests.get(url)
-
 # Convert response data to JSON format
 data = res.json()
-
 # Check if the request was successful
 if res.status_code == 200:
     print("City:", data["name"])  # Print city name
