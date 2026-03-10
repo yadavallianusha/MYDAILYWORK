fa312a0b21a664f35c859719b01c263c0ea976c5
Yadavalli Anusha <yadavallianusha019@gmail.com>
2026-03-10 07:35:23 +0530

    Update todo.py

--- Task1_TodoList/todo.py
+++ Task1_TodoList/todo.py
@@ -1,31 +1,41 @@
+# Simple To-Do List Program
 todo_list = []
-
 while True:
-    print("\n1. Add Task")
+    # Display menu options
+    print("\n----- TO DO LIST MENU -----")
+    print("1. Add Task")
     print("2. View Tasks")
     print("3. Remove Task")
     print("4. Exit")
-
     choice = input("Enter your choice: ")
-
+    # Add a new task
     if choice == "1":
         task = input("Enter task: ")
         todo_list.append(task)
         print("Task added successfully!")
-
+    # View all tasks
     elif choice == "2":
-        print("\nYour Tasks:")
-        for i, task in enumerate(todo_list, start=1):
-            print(i, task)
-
+        if not todo_list:
+            print("No tasks in the list.")
+        else:
+            print("\nYour Tasks:")
+            for i, task in enumerate(todo_list, start=1):
+                print(i, task)
+    # Remove a task
     elif choice == "3":
-        num = int(input("Enter task number to remove: "))
-        todo_list.pop(num - 1)
-        print("Task removed!")
-
+        if not todo_list:
+            print("No tasks to remove.")
+        else:
+            num = int(input("Enter task number to remove: "))
+            if 1 <= num <= len(todo_list):
+                removed_task = todo_list.pop(num - 1)
+                print("Removed task:", removed_task)
+            else:
+                print("Invalid task number!")
+    # Exit program
     elif choice == "4":
         print("Thank you! Exiting...")
         break
-
+    # Invalid input
     else:
-        print("Invalid choice")
+        print("Invalid choice! Please enter 1-4.")
