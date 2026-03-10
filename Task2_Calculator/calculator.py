9b02d14c2794d12485b1fb6b5b3184ea4812c5ac
Yadavalli Anusha <yadavallianusha019@gmail.com>
2026-03-10 07:41:58 +0530

    Update calculator.py

--- Task2_Calculator/calculator.py
+++ Task2_Calculator/calculator.py
@@ -1,25 +1,29 @@
+# Simple Calculator Program
+# Function to perform calculation
+def calculate(num1, num2, choice):
+    if choice == "1":
+        return num1 + num2
+    elif choice == "2":
+        return num1 - num2
+    elif choice == "3":
+        return num1 * num2
+    elif choice == "4":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero is not allowed"
+    else:
+        return "Invalid choice"
+# Take input numbers
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-
-print("Choose operation")
+# Show menu
+print("\nChoose operation")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
-
 choice = input("Enter choice (1/2/3/4): ")
-
-if choice == "1":
-    print("Result:", num1 + num2) # add the num
-elif choice == "2":
-    print("Result:", num1 - num2) # Subtract two numbers
-elif choice == "3":
-    print("Result:", num1 * num2)# Multiply two numbers
-
-elif choice == "4":
-    if num2 != 0:
-        print("Result:", num1 / num2)  # Divide two numbers
-    else:
-        print("Error: Division by zero is not allowed")
-else:
-    print("Invalid choice")
+# Call function
+result = calculate(num1, num2, choice)
+print("Result:", result)
