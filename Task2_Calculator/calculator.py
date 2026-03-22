1056a1faed83cf22a7f759de5a1f9333589aaad7
Yadavalli Anusha <yadavallianusha019@gmail.com>
2026-03-22 15:49:20 +0530

    Update calculator.py

--- Task2_Calculator/calculator.py
+++ Task2_Calculator/calculator.py
@@ -17,26 +17,27 @@ def calculate(num1, num2, choice):
     else:
         return "Invalid choice"
 
-try:
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-except ValueError:
-    print("Invalid input! Enter numbers only.")
-    exit()
+num1 = 10
+num2 = 5
+choice = "1"   # ✅ fixed (no input)
+
 print("\nChoose operation")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
-choice = input("Enter choice (1/2/3/4): ")
-# Measure only the calculation
+
+# Measure calculation
 start_time = time.perf_counter()
 result = calculate(num1, num2, choice)
 end_time = time.perf_counter()
+
 print("Result:", result)
+
 execution_time = end_time - start_time
 process = psutil.Process(os.getpid())
 memory_usage = process.memory_info().rss / 1024 / 1024
+
 print("\nPerformance Metrics")
 print("Execution Time:", round(execution_time, 8), "seconds")
 print("Memory Usage:", round(memory_usage, 2), "MB")
